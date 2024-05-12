158703354c0ca2650ae25dbdf16100e18453ff60
Daniel Bartolini <djb3149@gmail.com>
2024-05-11 21:44:40 -0400

    commit

---
+++ build.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import platform
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Compiling project . . . . . .")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# Compile command
+compile_line = os.system("clang++ main.cpp -o sound -I/opt/homebrew/opt/sfml/include -L/opt/homebrew/opt/sfml/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio")
+
+# Compile flag
+if compile_line == 0:
+    print("~~~~~~~~~~~~~~~~~~~~~")
+    print("Compiled successfully")
+    print("~~~~~~~~~~~~~~~~~~~~~")
+
+    # Determining platform
+    namestr = platform.system()
+
+    if namestr == 'Linux':
+        platform = 'linux'
+
+    elif namestr == 'Darwin':
+        platform = 'macos'
+
+    # Command alias
+    alias_line = f"alias generate='{os.path.abspath('./sound')}'"
+
+    shell_profile = os.path.expanduser("~/.bashrc" if platform == 'linux' else "~/.bash_profile")
+    if not any("sound" in line for line in open(shell_profile)):
+        with open(shell_profile, "a") as profile:
+            profile.write(alias_line + "\n")
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "sound.h"
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Sine Wave Oscillator");
+
+    sf::SoundBuffer buffer;
+    vector<sf::Int16> samples;
+
+    sf::Sound silence;
+
+
+    for (int i=0; i < 44100 * 0.5; i++)       // 0.5 seconds worth of sound
+    {
+        samples.push_back(sound::SineWave(i, 392, 0.9));
+    }
+
+    for (int i=0; i < 44100 * 0.5; i++)       // 0.5 seconds worth of sound
+    {
+        samples.push_back(sound::SquareWave(i, 392, 0.9));
+    }
+
+    for (int i=0; i < 44100 * 1; i++)       // 1 second worth of sound
+    {
+        samples.push_back(sound::Noise(0.9));
+    }
+
+
+
+    if (!buffer.loadFromSamples(&samples[0], samples.size(), 2, 44100))
+    {
+        // Error loading buffer
+        return 1;
+    }
+
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+
+    if (sound.getStatus() != sf::Sound::Playing)
+    {
+        sound.play();
+    }
+
+    printf("AUDIO STATUS: %i\n ", sound.getStatus());
+
+
+    if (sound.getStatus() != sf::Sound::Playing)
+    {
+        // Error playing sound
+        return 1;
+    }
+
+    // Game Loop
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        // printf("AUDIO STATUS: %i\n", sound.getStatus());
+
+        while(window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
+
+    return 0;
+}
